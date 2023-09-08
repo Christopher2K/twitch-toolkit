1ad3e77dc73e90dabdd3d0ae94667f103ba24ec2
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-09-08 18:17:50 -0400

    chore: use panda + park ui

--- apps/control/vite.config.ts
+++ apps/control/vite.config.ts
@@ -1,9 +1,16 @@
+import path from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@style': path.resolve(__dirname, 'styled-system'),
+    },
+  },
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   //
