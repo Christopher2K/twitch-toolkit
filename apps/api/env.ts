7b224c8239e049b34cac0b1aa0a0e4a6cb4d671a
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-09-10 15:16:18 -0400

    chore: add sessions package

--- apps/api/env.ts
+++ apps/api/env.ts
@@ -20,6 +20,7 @@ export default Env.rules({
 	APP_KEY: Env.schema.string(),
 	APP_NAME: Env.schema.string(),
   DRIVE_DISK: Env.schema.enum(['local'] as const),
+  SESSION_DRIVER: Env.schema.enum(['cookie'] as const),
 	NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
   DATABASE_URL: Env.schema.string(),
 })
