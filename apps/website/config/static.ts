b36fdae273c5526c9de69ca83a2af93fdfc4e593
Christopher N. KATOYI KABA <me@christopher2k.dev>
2023-09-11 16:05:30 -0400

    fix: static config

---
+++ apps/website/config/static.ts
@@ -0,0 +1,11 @@
+import { AssetsConfig } from '@ioc:Adonis/Core/Static'
+
+const staticConfig: AssetsConfig = {
+  enabled: true,
+  dotFiles: 'ignore',
+  etag: true,
+  lastModified: true,
+}
+
+export default staticConfig
+
